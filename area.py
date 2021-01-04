4ed616418c4985db552dd72e2eb7e30783e1f251
sakshamd84 <61703740+sakshamd84@users.noreply.github.com>
2021-01-04 19:14:29 +0530

    Python-mycaption

---
+++ area.py
@@ -0,0 +1,11 @@
+r = float(input("Input the radius of the circle : "));
+area = (float)(22/7) * (float)(r) * (float)(r);
+print("The area of the circle with radius ",r,"is",area);
+
+a=input("Enter the name of the file : ");
+if ".py" in a:
+    print("Python");
+if ".c" in a:
+    print("C");
+if ".cpp" in a:
+    print("C++");
---
+++ filename.py
@@ -0,0 +1,7 @@
+a=input("Enter the name of the file : ");
+if ".py" in a:
+    print("Python");
+if ".c" in a:
+    print("C");
+if ".cpp" in a:
+    print("C++");
